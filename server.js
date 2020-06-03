de9c41fba30507898fa6112704ae88a87c714a35
Raghav Agarwal <raghav18177@iiitd.ac.in>
2020-06-04 00:24:36 +0530

    Heroku server
    Signed-off-by: Raghav Agarwal <raghav18177@iiitd.ac.in>

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express') ;
 const app = express() ;
 
+const PORT = process.env.PORT || 4444 ;
+
 app.get('/', (req,res)=> {
     res.send('Hello There !!')
 })
 
-app.listen(4444, (req,res)=> {
-    console.log("Server started on http://localhost:4444")
+app.listen(PORT, (req,res)=> {
+    console.log(`Server started on http://localhost:${PORT}`)
 })
